ab3b01f1b457a521a6ac2ee6c631ecf92524544a
gio <giorodriguez021@gmail.com>
2018-07-18 14:40:59 +0800

    Added about us

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ class About extends Component {
         <div className="row">
             <div className="col-md-12" id="about">
               <h3>About Me</h3>
-              <p></p>
+              <p>I am a dedicated full stack web developer and out of all the many fields in the IT world, web development is my biggest strength and passion. I am adept both in the front-end and back-end web development. I specialized in JavaScript and PHP as my primary languages. I am also interested in learning about mobile development. I am a fast learner and very eager about learning many things. I am also fond of rock music and play the guitar. Basketball is also one of my hobbies and I am a big fan of the NBA especially the team of Spurs and Lakers. Aside from that, I like comedy series and movies notably 'Silcon Valley', 'Brooklyn Nine Nine', 'The Big Bang Theory' and 'The Hangover' film series.</p>
             </div>
         </div>
       </div>
